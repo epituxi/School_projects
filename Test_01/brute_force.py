a0c2d6fe12a56e6094515d1a9f306fb12b93f4cb
epituxi <21289842+epituxi@users.noreply.github.com>
2021-10-27 11:49:49 +0300

    Nice little timer added

--- Test_01/brute_force.py
+++ Test_01/brute_force.py
@@ -1,6 +1,8 @@
+import datetime
 import string
 import itertools
 from timeit import default_timer as timer
+import time
 
 #This program brute forces a password
 
@@ -9,20 +11,24 @@ def password():
     return password
 
 def solver(password):
+    global guess
     i = 0
     chars = string.printable
     answer = ''
     time_start = timer()
+    sekuntikello = int(timer() - time_start)
     for j in range(1, 100000000):
         for guess in itertools.product(chars, repeat=j):
             i += 1
             guess = ''.join(guess)
+            if sekuntikello < int(timer() - time_start):
+                sekuntikello = int(timer() - time_start)
+                print(guess, sekuntikello)
             if guess == password:
                 time_end = timer()
                 answer = password
                 return answer, time_end, time_start
 
-        
 
 def main():
     salasana = password()
