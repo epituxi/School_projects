8c092ac203ac1a94b7316bc4ae95ae05b1f24e5c
epituxi <21289842+epituxi@users.noreply.github.com>
2021-11-03 16:15:30 +0200

    2

--- Assignment6_4/monty_hall.py
+++ Assignment6_4/monty_hall.py
@@ -25,29 +25,34 @@ def initialize_doors(number_of_doors):
 
 def remove_wrong_doors(chosen_door, doors):
     # Implement your code here
+
+    
     pass
 
 def print_doors(doors, dont_open):
     # Implement your code here
     number_of_doors = len(doors)
-    door_number_list = [n for n in range(1, number_of_doors + 1)]
-    print(" _  " * number_of_doors)
-    print("| | " * number_of_doors)
-    print("|_| " * number_of_doors)
-    for i in range(number_of_doors):
-        print("{:^3.0f} ".format(i + 1), end="")
+    if dont_open == 0:
+        print(" _  " * number_of_doors)
+        print("| | " * number_of_doors)
+        print("|_| " * number_of_doors)
+        for i in range(number_of_doors):
+            print("{:^3.0f} ".format(i + 1), end="")
+
+    
     pass
 
 def main():
     seed = int(input("Set seed:\n"))
     random.seed(seed)
     # Implement your code here
-    dont_open = 0
+    chosen_door = 0
     number_of_doors = amount_of_doors()
     doors = initialize_doors(number_of_doors)
-    door_printer = print_doors(doors, dont_open)
+    door_printer = print_doors(doors, chosen_door)
     chosen_door = door_choice(number_of_doors)
-    print(f"You chose the door number {chosen_door}.\n...")
+    print(f"You chose the door number {chosen_door}.\n")
+    remove_wrong_doors(chosen_door, doors)
 
 
 main()
