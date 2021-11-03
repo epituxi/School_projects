74653699614bccccb3762b74478161b4c830b82b
epituxi <21289842+epituxi@users.noreply.github.com>
2021-11-03 12:17:42 +0200

    init

---
+++ Assignment6_4/monty_hall.py
@@ -0,0 +1,21 @@
+import random
+
+def initialize_doors(number_of_doors):
+    # Implement your code here
+    pass
+
+def remove_wrong_doors(chosen_door, doors):
+    # Implement your code here
+    pass
+
+def print_doors(doors, dont_open):
+    # Implement your code here
+    pass
+
+def main():
+    seed = int(input("Set seed:\n"))
+    random.seed(seed)
+    
+    # Implement your code here
+
+main()
